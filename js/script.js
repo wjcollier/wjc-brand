d401e674fa90d528c09fca140aba3408ab04bfb3
Billy_Joe <williamjcollier4@gmail.com>
2023-06-02 15:18:20 +0800

    updated images

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 let menu = document.querySelector('#menu-bar')
 let navbar = document.querySelector('.navbar')
+let body = document.querySelector('body')
 
-menu.onclick = () => {
+menu.onclick = (event) => {
+  event.stopPropagation() // Stop the click event from propagating to the body element
   navbar.classList.toggle('active')
   menu.classList.toggle('fa-times')
+  if (navbar.classList.contains('active')) {
+    body.classList.add('menu-open') // Add a class to the body when the menu is open
+  } else {
+    body.classList.remove('menu-open') // Remove the class from the body when the menu is closed
+  }
+}
+
+// Close the mobile menu when clicking anywhere on the screen
+body.onclick = (event) => {
+  if (!navbar.contains(event.target)) {
+    navbar.classList.remove('active')
+    menu.classList.remove('fa-times')
+    body.classList.remove('menu-open')
+  }
 }
 
 window.onscroll = () => {
@@ -49,3 +65,41 @@ var swiper = new Swiper('.testimonials-slider', {
   grabCursor: true
 })
 
+
+/**
+ * Custom cursor
+ */
+
+const cursors = document.querySelectorAll("[data-cursor]");
+const hoveredElements = [...document.querySelectorAll("button"), ...document.querySelectorAll("a")];
+
+window.addEventListener("mousemove", function (event) {
+
+  const posX = event.clientX;
+  const posY = event.clientY;
+
+  /** cursor dot position */
+  cursors[0].style.left = `${posX}px`;
+  cursors[0].style.top = `${posY}px`;
+
+  /** cursor outline position */
+  setTimeout(function () {
+    cursors[1].style.left = `${posX}px`;
+    cursors[1].style.top = `${posY}px`;
+  }, 80);
+
+});
+
+/** add hovered class when mouseover on hoverElements */
+addEventOnElements(hoveredElements, "mouseover", function () {
+  for (let i = 0, len = cursors.length; i < len; i++) {
+    cursors[i].classList.add("hovered");
+  }
+});
+
+/** remove hovered class when mouseout on hoverElements */
+addEventOnElements(hoveredElements, "mouseout", function () {
+  for (let i = 0, len = cursors.length; i < len; i++) {
+    cursors[i].classList.remove("hovered");
+  }
+});
